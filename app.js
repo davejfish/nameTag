aa78dd1af5a8ed1d111992a094095f5e738d5864
Dave Fisher <dave.j.fish@gmail.com>
2022-05-31 15:53:01 -0700

    removed log testing for inputDisplay element

--- app.js
+++ app.js
@@ -3,8 +3,6 @@
 const nameInput = document.getElementById('nameInput');
 const inputDisplay = document.getElementById('inputDisplay');
 
-console.log(inputDisplay)
-
 nameInput.addEventListener('input', () => {
     const name = nameInput.value;
 });
