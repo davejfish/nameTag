77923fd11aceb27bf10cb7e8e6c7fb8bdad8863f
Dave Fisher <dave.j.fish@gmail.com>
2022-05-31 17:35:57 -0700

    added and verified dropdown box for choice of animal

--- app.js
+++ app.js
@@ -6,18 +6,23 @@ const pronounInput = document.getElementById('pronounInput');
 const makeName = document.getElementById('nameButton');
 
 const nametagColor = document.getElementById('color');
+const nametag = document.getElementById('nametag');
 
+const image = document.getElementById('image');
+const animal = document.getElementById('animal');
 
 makeName.addEventListener('click', () => {
     const name = nameInput.value;
     const pronoun = pronounInput.value;
     inputDisplay.textContent = `${name} (${pronoun})`;
+    nametag.style.backgroundColor = nametagColor.value;
+    image.src = animal.value;
 });
 
-
-
+// nametagColor.addEventListener()
 
 // set event listeners
     // get info from user input
     // use user input to update state
     // update DOM to reflect the new state
+
